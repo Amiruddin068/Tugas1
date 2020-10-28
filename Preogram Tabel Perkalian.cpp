4157b2b812e6d34fa4b7354e6607c1ce920ac672
Amiruddin068 <72743689+Amiruddin068@users.noreply.github.com>
2020-10-29 02:34:56 +0700

    Add files via upload

---
+++ Preogram Tabel Perkalian.cpp
@@ -0,0 +1,29 @@
+/* Program Tabel Perkalian */
+/* Muhammad Imam Amiruddin */
+/* 20051397068 */
+/* 2020B */
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+
+main()
+{
+int i,j;
+cout<<"Tabel Perkalian \n";
+
+
+for(i=1;i<=10;i++)
+{cout<<" "<<i;
+
+
+for(j=1;j<=10;j++)
+cout<<" "<<i*j;
+cout<<endl;
+}
+
+getch();
+
+}
